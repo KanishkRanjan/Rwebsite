f199be5311d33aac3bb6586f18379c4947847c91
Kanishk <kanishkranjan17@gmail.com>
2025-01-12 17:47:53 +0530

    fixed cses scrapper

--- src/utils/scrapper/cses.scrapper.ts
+++ src/utils/scrapper/cses.scrapper.ts
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import cheerio from 'cheerio';
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 require('dotenv').config();
 
@@ -9,12 +9,15 @@ async function fetchCSESData(userId:string) {
     const cookies = {
         PHPSESSID: process.env.PHPSESSID || "0e23d626ace5d7aedfcf67437ec9ceff726f9500"
     };
+    // console.log(process.env.PHPSESSID)
     try {
         const response = await axios.get(`https://cses.fi/problemset/user/${userId}/`, {
             headers: {
                 Cookie:  `PHPSESSID=${cookies.PHPSESSID}`,
             },
         });
+    
+        console.log(cheerio)
 
         const $ = cheerio.load(response.data);
 
@@ -26,43 +29,5 @@ async function fetchCSESData(userId:string) {
         return { success : false}
     }
 }
-
-// async function fetchCSESData() {
-//     const cookies = {
-//         PHPSESSID: process.env.PHPSESSID || "0e23d626ace5d7aedfcf67437ec9ceff726f9500"
-//     };
-
-//     const headers = {
-//         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-//         'Accept-Language': 'en-US,en;q=0.9',
-//         'Cookie': `PHPSESSID=${cookies.PHPSESSID}`,
-//         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36'
-//     };
-
-//     const users = {};
-//     try {
-//         for (let page of [1, 2]) {
-//             const response = await axios.get(`https://cses.fi/problemset/stats/friends/p/${page}`, {
-//                 headers,
-//                 timeout: 10000,
-//                 validateStatus: (status:number) => status === 200
-//             });
-            
-            
-
-//             if (response.data) {
-//                 const $ = cheerio.load(response.data);
-//                 Object.assign(users, getUsers($));
-//             }
-//         }
-        
-//         return Object.keys(users).length > 0 ? users : null;
-//     } catch (error) {
-//         console.error('Error fetching CSES data:', (error as Error).message);
-//         return null;
-//     }
-
-    
-// }
 
 export default fetchCSESData;
